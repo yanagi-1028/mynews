a19f046ae0731ebac0be960439451291e50de6ef
EC2 Default User <ec2-user@ip-172-31-17-231.us-east-2.compute.internal>
2020-01-01 15:18:34 +0000

    third commit

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group( ['prefix'=> 'admin' ], function(){
-    Route::get('news/create','Admin\NewsController@add');
-    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit','Admin\ProfileController@edit');
+
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
