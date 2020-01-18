db00c90eff66b609b2a260a43031ad627d724832
EC2 Default User <ec2-user@ip-172-31-17-231.us-east-2.compute.internal>
2020-01-18 13:57:38 +0000

    seventh commit

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -16,6 +16,7 @@
                                 </div>
                                 <div class="title p-2">
                                     <h1>{{ str_limit($headline->title, 70) }}</h1>
+                                    <p>追加開発しました。</p>
                                 </div>
                             </div>
                         </div>
